d9cf10b514ff5e406e683af66c85fa0616980b84
jemmx8iso <jemmx8iso@gmx.com>
2025-03-04 12:14:43 +0000

    Add 1.cpp

---
+++ src/1.cpp
@@ -0,0 +1,9 @@
+int main() {
+  // Generate a random number between 1 and 10
+  int random_number = rand() % 10 + 1;
+
+  // Print the random number
+  std::cout << "The random number is: " << random_number << std::endl;
+
+  return 0;
+}
